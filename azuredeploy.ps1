69e7e580f846b063882b59acc4f946e3c5478dbe
Roman Velic <roman.velic@gmail.com>
2018-06-13 14:19:36 +0800

    Tag resource group
    Ability to tag deployed resource group with hashtable of tags. Previous tags are always rewritten

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -7,6 +7,7 @@ Param(
     [string] [Parameter(Mandatory=$true)] $StagingDirectory,
     [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [hashtable] $ResourceGroupTags,
     [string] $TemplateFile = $StagingDirectory + '\azuredeploy.json',
     [string] $TemplateParametersFile = $StagingDirectory + '\azuredeploy.parameters.json',
     [switch] $ValidateOnly,
@@ -66,5 +67,10 @@ else {
                                        -ErrorVariable ErrorMessages
     if ($ErrorMessages) {
         Write-Output '', 'Template deployment returned the following errors:', @(@($ErrorMessages) | ForEach-Object { $_.Exception.Message.TrimEnd("`r`n") })
+    }
+
+    if ($ResourceGroupTags) {
+      Write-Host "Tagging resource group: $ResourceGroupName"
+      Set-AzureRmResourceGroup -Name $ResourceGroupName -Tag $ResourceGroupTags
     }
 }
